427ccc33f37b25de5d23b1399fbccf72acb726a1
Winston Chang <winston@stdout.org>
2019-03-07 16:09:07 -0600

    Iniital commit

---
+++ R/webshot.R
@@ -0,0 +1,70 @@
+#' @import chromote
+#' @import later
+#' @import promises
+#'
+NULL
+
+
+#' @param
+webshot <- function(
+  url = NULL,
+  file = "webshot.png",
+  vwidth = 992,
+  vheight = 744,
+  cliprect = NULL,
+  selector = NULL,
+  expand = NULL,
+  delay = 0.2,
+  zoom = 1,
+  eval = NULL,
+  debug = FALSE,
+  useragent = NULL
+) {
+
+  if (length(url) == 0) {
+    stop("Need url.")
+  }
+
+  # If user provides only one file name but wants several screenshots, then the
+  # below code generates as many file names as URLs following the pattern
+  # "filename001.png", "filename002.png", ... (or whatever extension it is)
+  if (length(url) > 1 && length(file) == 1) {
+    file <- vapply(1:length(url), FUN.VALUE = character(1), function(i) {
+      replacement <- sprintf("%03d.\\1", i)
+      gsub("\\.(.{3,4})$", replacement, file)
+    })
+  }
+
+  cm <- ChromoteMaster$new()
+
+  # A list of promises for the screenshots
+  res <- mapply(url, file, FUN = function(url, file) {
+    new_session_screenshot(cm, url, file)
+  })
+
+
+  p <- do.call("promise_all", res)
+  cm$wait_for(p)
+}
+
+
+new_session_screenshot <- function(chromote_master, url, filename) {
+  s <- NULL
+
+  p <- chromote_master$new_session(sync_ = FALSE)
+  p$then(function(session) {
+      s <<- session
+      s$Page$navigate(url, sync_ = FALSE)
+      s$Page$loadEventFired(sync_ = FALSE)
+    })$
+    then(function(value) {
+      message(url, " loaded")
+      s$screenshot(filename = filename, show = FALSE, scale = 0.5)
+    })$
+    then(function(value) {
+      message(url, " screenshot completed")
+    })$
+    finally(function() {
+      s$close()
+    })
+}
