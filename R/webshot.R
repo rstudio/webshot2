6f7409c778f4905e879698e81700d990989b0657
Winston Chang <winston@stdout.org>
2019-07-23 15:59:08 -0500

    Better handling of NULL selectors

--- R/webshot.R
+++ R/webshot.R
@@ -192,7 +192,9 @@ new_session_screenshot <- function(
   zoom
 ) {
 
-  if (!is.null(selector) && length(selector) != 1) {
+  if (is.null(selector)) {
+    selector <- "html"
+  } else if (!is.null(selector) && length(selector) != 1) {
     stop("`selector` must be single string")
   }
 
@@ -236,7 +238,7 @@ new_session_screenshot <- function(
     })$
     then(function(value) {
       s$screenshot(
-        selector = selector, cliprect = cliprect, filename = file,
+        filename = file, selector = selector, cliprect = cliprect,
         expand = expand, scale = zoom,
         show = FALSE, sync_ = FALSE
       )
