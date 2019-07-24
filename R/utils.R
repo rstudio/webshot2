945f425304b5049760b00f5eaf72d3518b749e4d
Winston Chang <winston@stdout.org>
2019-07-23 21:39:11 -0500

    Bring in more code from webshot

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,29 @@ long_to_wide <- function(x) {
     })
   })
 }
+
+
+# Find an available TCP port (to launch Shiny apps)
+available_port <- function(port = NULL, min = 3000, max = 9000) {
+  if (!is.null(port)) return(port)
+
+  # Unsafe port list from shiny::runApp()
+  valid_ports <- setdiff(min:max, c(3659, 4045, 6000, 6665:6669, 6697))
+
+  # Try up to 20 ports
+  for (port in sample(valid_ports, 20)) {
+    handle <- NULL
+
+    # Check if port is open
+    tryCatch(
+      handle <- httpuv::startServer("127.0.0.1", port, list()),
+      error = function(e) { }
+    )
+    if (!is.null(handle)) {
+      httpuv::stopServer(handle)
+      return(port)
+    }
+  }
+
+  stop("Cannot find an available port")
+}
