15e77a4db24e863ddf408c60393acc2ff0632a0d
Garrick Aden-Buie <garrick@adenbuie.com>
2025-04-16 16:06:44 -0400

    chore: only deparse if needed

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 as_rounded_int <- function(x) {
-  x_name <- deparse(substitute(x))
   if (!is.numeric(x)) {
+    x_name <- deparse(substitute(x))
     stop("`", x_name, "` must be an integer number of pixels, not '", x, "'.")
   }
   if (inherits(x, "AsIs")) x else round(x, digits = 0)
